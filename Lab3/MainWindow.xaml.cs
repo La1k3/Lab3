9c055b804bb86a3ad1eab72a1cf964cf3c49e525
Brajan Wójcik <brajanwojcik@wp.pl>
2023-12-07 18:28:53 +0100

    Gramatyka git ale ten kod słabo

--- Lab3/MainWindow.xaml.cs
+++ Lab3/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Speech.Recognition;
+using Microsoft.Speech.Synthesis;
+using System.Globalization;
 
 namespace Lab3
 {
@@ -20,9 +23,104 @@ namespace Lab3
     /// </summary>
     public partial class MainWindow : Window
     {
+        static SpeechSynthesizer ss;
+        static SpeechRecognitionEngine sre;
+        static bool done = false;
         public MainWindow()
         {
+            ss = new SpeechSynthesizer();
+            ss.SetOutputToDefaultAudioDevice();
+            ss.Speak("Witam w salonie samochodowym");
+            ss.Speak("Jaki samochód Pan sobie życzy");
+            CultureInfo ci = new CultureInfo("pl-PL");
+            sre = new SpeechRecognitionEngine(ci);
+            sre.SetInputToDefaultAudioDevice();
+            sre.SpeechRecognized += Sre_SpeechRecognized;
+            
+            Grammar grammar = new Grammar(".\\Grammars\\Grammar1.xml", "rootRule");
+            grammar.Enabled = true;
+            sre.LoadGrammar(grammar);
+            sre.RecognizeAsync(RecognizeMode.Multiple);
             InitializeComponent();
+        }
+
+        private void Sre_SpeechRecognized1(object sender, SpeechRecognizedEventArgs e)
+        {
+            float confidence = e.Result.Confidence;
+            List<TextBox> textBoxesToChange = new List<TextBox> { brand1, color1, fuel1 };
+            if (confidence > 0.7)
+            {
+                if (e.Result.Semantics.ContainsKey("color"))
+                {
+                    string color = e.Result.Semantics["color"].Value.ToString();
+                    color1.Text = color;
+                }
+                else
+                {
+                    ss.Speak("Proszę podać kolor samochodu");
+                }
+                if (e.Result.Semantics.ContainsKey("fuel"))
+                {
+                    string fuel = e.Result.Semantics["fuel"].Value.ToString();
+                    fuel1.Text = fuel;
+                }
+                else
+                {
+                    ss.Speak("Proszę podać rodzaj paliwa samochodu");
+                }
+            }
+            else
+            {
+                ss.Speak("Proszę powtórzyć");
+                wynik.Text = "Proszę powtórzyć, ponieważ rozpoznawanie wynosi: " + confidence;
+            }
+        }
+
+        private void Sre_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            float confidence = e.Result.Confidence;
+            List<TextBox> textBoxesToChange = new List<TextBox> { brand1, color1, fuel1 };
+            if (confidence > 0.7)
+            {
+                foreach (var textBox in textBoxesToChange)
+                {
+                    textBox.Text = null;
+                }
+                if (e.Result.Semantics.ContainsKey("brand"))
+                {
+                    string brand = e.Result.Semantics["brand"].Value.ToString();
+                    brand1.Text = brand;
+                    ss.Speak("Proszę podać kolor i rodzaj paliwa samochodu");
+                    sre.SpeechRecognized += Sre_SpeechRecognized1;
+                }
+                else
+                {
+                    ss.Speak("Proszę podać markę samochodu");
+                }
+                if (e.Result.Semantics.ContainsKey("color"))
+                {
+                    string color = e.Result.Semantics["color"].Value.ToString();
+                    color1.Text = color;
+                }
+                else
+                {
+                    ss.Speak("Proszę podać kolor samochodu");
+                }
+                if (e.Result.Semantics.ContainsKey("fuel"))
+                {
+                    string fuel = e.Result.Semantics["fuel"].Value.ToString();
+                    fuel1.Text = fuel;
+                }
+                else
+                {
+                    ss.Speak("Proszę podać rodzaj paliwa samochodu");
+                }
+            }
+            else
+            {
+                ss.Speak("Proszę powtórzyć");
+                wynik.Text = "Proszę powtórzyć, ponieważ rozpoznawanie wynosi: " + confidence;
+            }
         }
     }
 }
